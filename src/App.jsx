5859e65292a97e6fa5c3d09d953b979648174ed2
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-01-21 19:51:16 -0800

    start contact app using reactjs and sass

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header.jsx"
+function App() {
+
+  return (
+    <>
+     <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './header.module.scss';
+
+function Header() {
+    return (
+        <>
+        <div>
+            <h1>Contact App</h1>
+        </div>
+        <p><a href="#">RohaDeveloper</a> | All things you want</p>
+        </>
+    );
+}
+
+export default Header;
